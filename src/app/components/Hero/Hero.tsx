de9132e16e90d52696e7e386d9c3ddd3a6f01375
tinaaiscoding <104359991+tinaaiscoding@users.noreply.github.com>
2025-09-09 14:01:09 +1000

    Initial commit

---
+++ src/app/components/Hero/Hero.tsx
@@ -0,0 +1,7 @@
+import './Hero.css'
+
+export default function Hero() {
+  return (
+    <div className='hero_wrap'>Hero</div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from './components/Hero/Hero';
+
+export default function Home() {
+  return (
+    <div>
+      Hello world!
+      <Hero />
+    </div>
+  );
+}

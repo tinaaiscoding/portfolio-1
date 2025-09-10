a9c82163d0d6c30bb34d8028a683e08911e264a4
tinanvo <tinanhivo@gmail.com>
2025-09-10 11:02:31 +1000

    - Hero text parallax animation - Refactor

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,13 +1,41 @@
+'use client';
+
+import { useScroll } from 'framer-motion';
+import { useRef } from 'react';
+
 import PhraseWrap from '../PhraseWrap/PhraseWrap';
 import './Hero.css';
 
+const phrases = [
+  { phrase: 'Creative websites', left: '-45%', direction: 'left' },
+  { phrase: 'Creative websites', left: '-20%', direction: 'right' },
+  { phrase: 'Creative websites', left: '-35%', direction: 'left' },
+];
+
 export default function Hero() {
+  const heroContainer = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroContainer,
+    offset: ['start end', 'end start'],
+  });
+
   return (
-    <main className='flex items-end h-screen'>
+    <main
+      ref={heroContainer}
+      className='flex h-screen items-end overflow-hidden'
+    >
       <div>
-        <PhraseWrap phrase='Creative websites' />
-        <PhraseWrap phrase='Creative websites' />
-        <PhraseWrap phrase='Creative websites' />
+        {phrases.map((p, i) => {
+          return (
+            <PhraseWrap
+              key={i}
+              phrase={p.phrase}
+              left={p.left}
+              progress={scrollYProgress}
+              direction={p.direction}
+            />
+          );
+        })}
       </div>
     </main>
   );
--- src/app/components/PhraseWrap/PhraseWrap.tsx
+++ src/app/components/PhraseWrap/PhraseWrap.tsx
@@ -1,15 +1,34 @@
+import { MotionValue, motion, useTransform } from 'framer-motion';
+
 import Phrase from '../Phrase/Phrase';
 
 type Props = {
   phrase: string;
+  left: string;
+  progress: MotionValue<number>;
+  direction: string;
 };
 
-export default function PhraseWrap({ phrase }: Props) {
+export default function PhraseWrap({
+  phrase,
+  left,
+  progress,
+  direction,
+}: Props) {
+  const translateDir = direction === 'left' ? -1 : 1;
+  const translateX = useTransform(
+    progress,
+    [0, 1],
+    [250 * translateDir, -250 * translateDir],
+  );
   return (
-    <div className='flex gap-5 py-4 whitespace-nowrap'>
+    <motion.div
+      style={{ left, x: translateX }}
+      className='relative flex gap-5 py-4 whitespace-nowrap'
+    >
       <Phrase phrase={phrase} />
       <Phrase phrase={phrase} />
       <Phrase phrase={phrase} />
-    </div>
+    </motion.div>
   );
 }
